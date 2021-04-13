8aedf2b2f4b7c188acfa5e43aef49cf6c67b66c8
Norma Harary <normalenora@gmail.com>
2021-04-13 14:55:23 -0400

    saving test file

--- test_battleship.py
+++ test_battleship.py
@@ -1,11 +1,8 @@
 from battleshipPretty import create_board
 import pandas as pd
 from pandas import DataFrame
-import numpy as np
 
 def test_create_board():
-    np.random.seed(1)
-    board = DataFrame(np.random.rand()*100,
-               index=list('abcdefghij'),columns=list(range(1,11)), dtype=np.int64)
+    board = DataFrame(index=list('abcdefghij'),columns=list(range(1,11)))
     df = create_board()
     assert pd.testing.assert_frame_equal(board,df) == None
